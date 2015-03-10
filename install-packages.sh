15bc1628457a00747e93d4081804ea9bb1e72b14
Sam Mulube <sam@umbrellium.co.uk>
2015-03-10 12:50:24 +0000

    Try installing packages less noisily

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo apt-get update -y
-sudo apt-get install -y python-software-properties build-essential
-sudo apt-get install -y libgd2-xpm ia32-libs ia32-libs-multiarch
+sudo apt-get update -yq
+sudo apt-get install -yq python-software-properties build-essential
+sudo apt-get install -yq libgd2-xpm ia32-libs ia32-libs-multiarch
