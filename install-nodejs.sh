38cec6b931f113a9dd6e45bec03d1d4abdf62349
Sam Mulube <sam@umbrellium.co.uk>
2015-04-24 18:15:09 +0100

    Trying to get latest node so try building from source package.

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# Add repository
-curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get update -yq
-
-# Install nodejs
-sudo apt-get install -y nodejs
+# Download and build latest node.js
+curl -O http://nodejs.org/dist/v0.12.2/node-v0.12.2.tar.gz
+tar zxf node-v0.12.2.tar.gz
+cd node-v0.12.2
+./configure && make && sudo make install
 
 # Install global nodejs packages
 sudo npm install -g grunt-cli
