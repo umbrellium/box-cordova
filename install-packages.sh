78c5cf6e26b69585d03fa3768f6058585782fafe
Sam Mulube <sam@umbrellium.co.uk>
2015-04-29 18:18:11 +0100

    Install graphicsmagick and bump version number

--- install-packages.sh
+++ install-packages.sh
@@ -3,3 +3,4 @@
 sudo apt-get update -yq
 sudo apt-get install -yq python-software-properties build-essential
 sudo apt-get install -yq libgd2-xpm ia32-libs ia32-libs-multiarch
+sudo apt-get install -yq graphicsmagick
